e2cf865d0b618d1e9c7cee80cd9d428b3d85ce4f
k clair <kclair@riseup.net>
2012-10-04 14:06:42 -0700

    remove sympa-6.1.14 source

--- sympa-6.1.14-src/soap/sympa_soap_server-wrapper.fcgi.c
+++
@@ -1,8 +0,0 @@
-#include <unistd.h>
-
-int main(int argn, char **argv, char **envp) {
-    setreuid(geteuid(),geteuid());
-    setregid(getegid(),getegid());
-    argv[0] = SYMPASOAP;
-    execve(SYMPASOAP,argv,envp);
-}
